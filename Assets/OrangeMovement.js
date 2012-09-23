bd36fb75a7b87383c4724dbd138d4ed6a5900c13
Apelsin <appelsindrikken@gmail.com>
2012-09-23 00:03:23 -0700

    Making some headway. Gave the orange thing some controls. Camera is much nicer now, too. Let's get this party going.

--- Assets/OrangeMovement.js
+++ Assets/OrangeMovement.js
@@ -1,11 +1,16 @@
 
-var torqueMultiplier = 120;
 var forceMultiplier = 400;
 var velocityDamping = -1;
+var turnComp = 2.0;
+var realignmentDistance = 50;
 
-var speed = 800;
 var eulerAngleVelocity : Vector3 = Vector3 (0, 100, 0);
 
+var minVelOrient = 0.5;
+var dampingRate = 5.0;
+var rayLength = 10.0;
+//private var mask : int;
+private var currentUp = Vector3.up;
 
 function Start() {
 rigidbody.maxAngularVelocity = 5;
@@ -16,19 +21,55 @@ rigidbody.maxAngularVelocity = 5;
 function Update () {
 }
 
+function G6Curve(x)
+{
+    return 1.0 / Mathf.Pow(2.7183, Mathf.Pow(x, 6.0));
+}
+
 function FixedUpdate () {
 
-        //I need to somehow add drag that doesn't affect gravity.
-    rigidbody.AddRelativeForce (velocityDamping * rigidbody.velocity.x, -10, velocityDamping * rigidbody.velocity.z);
-    
-    rigidbody.AddRelativeForce (Vector3.forward * Input.GetAxis("Vertical") * forceMultiplier);
-    //rigidbody.AddRelativeForce (Vector3.right * Input.GetAxis("Horizontal") * horizontalForce);
-    
-    //rigidbody.AddRelativeTorque (Vector3.up * Input.GetAxis("Horizontal") * torqueMultiplier);
-    //rigidbody.AddRelativeTorque (Vector3.up * Input.GetAxis("Mouse X") * torqueMultiplier);
-    
-    var deltaRotation : Quaternion = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime * Input.GetAxis("Horizontal"));
-    rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
-    
+    var deltaRotation : Quaternion = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime * Input.GetAxis("Horizontal")); 
+    rigidbody.rotation *= deltaRotation;
+    // This assumes a and b are unit vectors
+    var deltaRotationInflection = 1.0 - Mathf.Pow(Vector3.Dot(deltaRotation * Vector3.fwd, Vector3.fwd), 1.0 + turnComp);
 
+    var hitA : RaycastHit;
+    var hitB : RaycastHit;
+    Physics.Raycast(transform.position, transform.forward - currentUp, hitA, rayLength, 0xFFFFFFFF);
+    if (hitA.normal != Vector3.zero) {
+        //currentUp = hitA.normal;
+    }
+    else
+        Debug.Log(hitA.normal);
+    
+    var targetRotation = transform.rotation;
+    if (rigidbody.velocity.magnitude > minVelOrient) {
+        //targetRotation = Quaternion.LookRotation (rigidbody.velocity, currentUp);
+        if (Physics.Raycast(transform.position + transform.forward, transform.forward - currentUp, hitB, rayLength, 0xFFFFFFFF)) {
+            //Debug.Log("DICKS");
+            targetRotation = Quaternion.LookRotation(hitB.point - hitA.point, hitA.normal);
+            //Debug.Log(targetRotation);
+        }
+    }
+    
+    
+    var euler = targetRotation.eulerAngles;
+    euler.y = rigidbody.rotation.eulerAngles.y;
+    
+    var pulldown = collider.bounds.size.y / 2.0 - hitA.distance;
+    
+    Debug.Log(pulldown);
+    
+    rigidbody.rotation = Quaternion.Slerp (
+        rigidbody.rotation, Quaternion.Euler(euler),
+        Time.deltaTime * dampingRate);
+    rigidbody.rotation = Quaternion.Slerp(
+        rigidbody.rotation,
+        Quaternion.Euler(0.0, rigidbody.rotation.eulerAngles.y, 0.0),
+        0.01 * (1.0 - G6Curve(pulldown / realignmentDistance)) * dampingRate);
+    rigidbody.AddRelativeForce (velocityDamping * rigidbody.velocity.x, -10, velocityDamping * rigidbody.velocity.z);
+    rigidbody.AddRelativeForce (Vector3.forward * Input.GetAxis("Vertical") * forceMultiplier);
+    rigidbody.AddRelativeForce (rigidbody.velocity * deltaRotationInflection);
+    rigidbody.AddForce(Vector3(0.0, pulldown, 0.0), ForceMode.Acceleration);
+    
 }
--- Assets/Standard Assets/Scripts/Camera Scripts/SmoothFollow.js
+++ Assets/Standard Assets/Scripts/Camera Scripts/SmoothFollow.js
@@ -1,7 +1,21 @@
+/*
+This camera smoothes out rotation around the y-axis and height.
+Horizontal Distance to the target is always fixed.
+
+There are many different ways to smooth the rotation but doing it this way gives you a lot of control over how the camera behaves.
+
+For every of those smoothed values we calculate the wanted value and the current value.
+Then we smooth it using the Lerp function.
+Then we apply the smoothed values to the transform's position.
+*/
+
+// The target we are following
 var target : Transform;
+// The distance in the x-z plane to the target
 var distance = 10.0;
-var height_hi = 5.0;
-var height_lo = 4.0;
+// the height we want the camera to be above the target
+var height = 5.0;
+// How much we 
 var heightDamping = 2.0;
 var rotationDamping = 3.0;
 
@@ -16,7 +30,7 @@ function LateUpdate () {
 	
 	// Calculate the current rotation angles
 	var wantedRotationAngle = target.eulerAngles.y;
-	var wantedHeight = target.position.y + height_hi;
+	var wantedHeight = target.position.y + height;
 		
 	var currentRotationAngle = transform.eulerAngles.y;
 	var currentHeight = transform.position.y;
@@ -36,7 +50,7 @@ function LateUpdate () {
 	transform.position -= currentRotation * Vector3.forward * distance;
 
 	// Set the height of the camera
-	transform.position.y = Mathf.Max(target.position.y + height_lo, currentHeight);
+	transform.position.y = currentHeight;
 	
 	// Always look at the target
 	transform.LookAt (target);
